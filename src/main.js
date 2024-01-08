1ad3ffa9b6a6deae926610cdc0afd1d3c986a0ac
silvaokindo <okindosilva@gmail.com>
2024-01-08 14:29:42 +0300

    first commit

---
+++ src/main.js
@@ -0,0 +1,6 @@
+
+import '../styles/utlis.css'
+import '../styles/components/aside.css'
+import '../styles/components/main.css'
+import '../styles/style.css'
+import '../styles/modern_normalizer.css'
